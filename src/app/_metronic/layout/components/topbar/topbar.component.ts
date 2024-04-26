a6ae61b48144c88e721e47f4c874db540cd295e6
Ivan Ivanov <ivanivamar99@gmail.com>
2024-04-26 10:54:10 +0200

    working on navbar

--- src/app/_metronic/layout/components/topbar/topbar.component.ts
+++ src/app/_metronic/layout/components/topbar/topbar.component.ts
@@ -1,21 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { LayoutService } from '../../core/layout.service';
+import {Component, OnInit} from '@angular/core';
+import {LayoutService} from '../../core/layout.service';
+import {MenuItem} from "primeng/api";
 
 @Component({
-  selector: 'app-topbar',
-  templateUrl: './topbar.component.html',
-  styleUrls: ['./topbar.component.scss'],
+    selector: 'app-topbar',
+    templateUrl: './topbar.component.html',
+    styleUrls: ['./topbar.component.scss'],
 })
 export class TopbarComponent implements OnInit {
-  toolbarButtonMarginClass = 'ms-1 ms-lg-3';
-  toolbarButtonHeightClass = 'w-30px h-30px w-md-40px h-md-40px';
-  toolbarUserAvatarHeightClass = 'symbol-30px symbol-md-40px';
-  toolbarButtonIconSizeClass = 'svg-icon-1';
-  headerLeft: string = 'menu';
+    toolbarButtonMarginClass = 'ms-1 ms-lg-3';
+    toolbarButtonHeightClass = 'w-30px h-30px w-md-40px h-md-40px';
+    toolbarUserAvatarHeightClass = 'symbol-30px symbol-md-40px';
+    toolbarButtonIconSizeClass = 'svg-icon-1';
+    headerLeft: string = 'menu';
+    items: MenuItem[] = [
+        {
+            label: 'Crear un Bot',
+            icon: 'pi pi-plus',
+            command: () => {
+            }
+        },
+        {
+            label: 'Mis Bots',
+            icon: 'pi pi-list',
+            command: () => {
+            }
+        }
+    ];
 
-  constructor(private layout: LayoutService) {}
+    constructor(private layout: LayoutService) {
+    }
 
-  ngOnInit(): void {
-    this.headerLeft = this.layout.getProp('header.left') as string;
-  }
+    ngOnInit(): void {
+        this.headerLeft = this.layout.getProp('header.left') as string;
+    }
 }
